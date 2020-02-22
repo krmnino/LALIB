5744444b3bef2c85493bd1eb23da313442ea6dd6
krmnino <kurt.manrique.n@gmail.com>
2020-02-22 18:10:33 -0500

    testing commits from vs2019

--- LinearAlgebraLibrary/LinearAlgebraLibrary.cpp
+++ LinearAlgebraLibrary/LinearAlgebraLibrary.cpp
@@ -1,5 +1,3 @@
-// LinearAlgebraLibrary.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <vector>
@@ -13,26 +11,21 @@
 
 using namespace std;
 
+void program_loop() {
+	while (true) {
+		//TODO
+	}
+}
+
 int main()
 {
 	//Matrix test("2 1 4; 0 1 1");
 	//Matrix test2("6 3 -1 0; 1 1 0 4;-2 5 0 2");
 	//Matrix test("3 4 2");
-	Matrix test("13; 9; 7; 15; 23; 6");
+	Matrix test("13; 9; 7; 15; 23; 7");
 	//Matrix test('r', 4);
 	//Matrix* test = new Matrix("13; 9; 7; 15; 23; 6");
 	cout << test.to_string() << endl;
 
 	cout << orthogonalize_vector(test).to_string() << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
