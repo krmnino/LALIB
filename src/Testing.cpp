d5a85c37ff7b44148247a72d5feec5472286db18
krmnino <kurt.manrique.n@gmail.com>
2021-10-03 02:25:03 -0400

    added test cases for is_singular()

--- src/Testing.cpp
+++ src/Testing.cpp
@@ -657,9 +657,9 @@ int main()
 	// Test: use determinant() on a 1 x 1 matrix -> succeeds
 	{
 		Matrix mtrx("5");
-		std::cout << mtrx << std::endl;
+		//std::cout << mtrx << std::endl;
 		auto ret = determinant(mtrx);
-		std::cout << ret << std::endl;
+		//std::cout << ret << std::endl;
 		assert(ret == 5);
 	}
 
@@ -679,7 +679,7 @@ int main()
 		assert(ret == 0);
 	}
 
-	// Test: use determinant() on a 5 x 5 singular matrix->succeeds
+	// Test: use determinant() on a 5 x 5 singular matrix -> succeeds
 	{
 		Matrix mtrx("1 2 3 4 5; 6 7 8 9 10; 11 12 13 14 15; 16 17 18 19 20; 21 22 23 24 25");
 		//std::cout << mtrx << std::endl;
@@ -719,6 +719,54 @@ int main()
 		//auto ret = determinant(mtrx);
 	}
 	
+	// Test: use is_singular() on a 3 x 3 -> succeeds
+	{
+		Matrix mtrx("1 2 3; 4 5 6; 7 8 9");
+		//std::cout << mtrx << std::endl;
+		assert(is_singular(mtrx));
+	}
+
+	// Test: use is_singular() on a 5 x 5 -> succeeds
+	{
+		Matrix mtrx("1 2 3 4 5; 6 7 8 9 10; 11 12 13 14 15; 16 17 18 19 20; 21 22 23 24 25");
+		//std::cout << mtrx << std::endl;
+		assert(is_singular(mtrx));
+	}
+
+	// Test: use is_singular() on a 4 x 4 matrix -> succeeds
+	{
+		Matrix mtrx("2 4 1 -3; 7 2 2 -2; 3 3 2 2; 0 5 1 0");
+		//std::cout << mtrx << std::endl;
+		assert(!is_singular(mtrx));
+	}
+
+	// Test: use is_singular() on a 5 x 5 matrix -> succeeds
+	{
+		Matrix mtrx("8 9 7 8 2; 4 5 9 5 7; 1 3 6 6 8; 4 5 7 4 5; 2 3 6 8 9");
+		//std::cout << mtrx << std::endl;
+		assert(!is_singular(mtrx));
+	}
+
+	// Test: use is_singular() on a 2 x 2 matrix -> succeeds
+	{
+		Matrix mtrx("5 8; 4 5");
+		//std::cout << mtrx << std::endl;
+		assert(!is_singular(mtrx));
+	}
+
+	// Test: use is_singular() on a 1 x 4 -> fails
+	{
+		Matrix mtrx("1 2 3 4");
+		//std::cout << mtrx << std::endl;
+		//auto ret = is_singular(mtrx);
+	}
+
+	// Test: use is_singular() on a 4 x 1 -> fails
+	{
+		Matrix mtrx("1; 2; 3; 4");
+		//std::cout << mtrx << std::endl;
+		//auto ret = is_singular(mtrx);
+	}
 
 	//Matrix b("0;1;6");
 	//Matrix c("0 2 0; 4 56 5; 4 5 6; 1 2 3; 1 2 6");
