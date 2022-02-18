1fcd089f0ffcc4565dbb3a79324eae4c465f7486
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2022-02-18 03:05:33 -0500

    renamed matrix minor variable to prevent use of keyword

--- src/Determinant.cpp
+++ src/Determinant.cpp
@@ -3,10 +3,10 @@
 
 namespace {
 	Matrix get_matrix_minor(Matrix& src, int skip_col) {
-		Matrix minor(src);
-		minor.remove_row(0);
-		minor.remove_column(skip_col);
-		return minor;
+		Matrix minor_mrtx(src);
+		minor_mrtx.remove_row(0);
+		minor_mrtx.remove_column(skip_col);
+		return minor_mrtx;
 	}
 
 	double recursive_determinant(Matrix& src) {
@@ -15,9 +15,9 @@ namespace {
 		if (src.get_m() == 1 && src.get_n() == 1) 
 			return src.get_single_element(0, 0);
 		for (int i = 0; i < src.get_n(); i++) {
-			Matrix minor(get_matrix_minor(src, i));
+			Matrix minor_mrtx(get_matrix_minor(src, i));
 			double cofactor = src.get_single_element(0, i);
-			accumulator += sign_flag * src.get_single_element(0, i) * recursive_determinant(minor);
+			accumulator += sign_flag * src.get_single_element(0, i) * recursive_determinant(minor_mrtx);
 			sign_flag = -sign_flag;
 		}
 		return accumulator;
